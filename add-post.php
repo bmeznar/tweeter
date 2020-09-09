e910993e459f72843620d547ed54c4b87d9a52db
bmeznar <bmeznar@users.noreply.github.com>
2020-09-09 09:55:46 +0200

    creating add-post.php

---
+++ add-post.php
@@ -0,0 +1,7 @@
+<div class="add_post">
+    <form class="post" action="index.php" method="post">
+      <input type="text" name="description" value="" required placeholder="What's on your mind?"><br><br>
+      <input type="file" name="image" value=""><br>
+      <input type="submit" name="post" value="Post">
+    </form>
+</div>
