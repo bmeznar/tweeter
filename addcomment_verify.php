b38ece291fea34bbc88d69a2019e855e5ab15c49
bmeznar <bmeznar@users.noreply.github.com>
2020-10-02 16:01:11 +0200

    redirect to post when commenting
    redirect to post you commented on when commenting

--- addcomment_verify.php
+++ addcomment_verify.php
@@ -5,5 +5,5 @@
   session_start();
   $sql = "INSERT INTO comments (text, post_id, user_id) VALUES (?,?,?)";
   $pdo->prepare($sql)->execute([$comment, $post_id, $_SESSION['id']]);
-  header('Location:index.php');
+  header('Location:index.php#post'.$_POST['post']);
 ?>
